b6ec1830d07932f86f763ac6cf99000e712bd052
PlagueDocBoi <plaguedocboi1@gmail.com>
2023-08-29 16:10:04 -0800

    Assignment code
    Hope I did this right

---
+++ Partnerintroduction/src/nameneeded/questions.java
@@ -0,0 +1,16 @@
+package nameneeded;
+
+public class questions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1 - Name, and major / reason for taking the class: My name is Karle Badder, I'm taking this class because I'm still trying to find out what I want to do in the future and being on the computer is one of my favorite things to do!");
+		System.out.println("2 - Any previous programming experience that they have: I have pretty much no programming skills, the closest experience I have is modding my video games; which I have been doing for like 6-7 years.");
+		System.out.println("3 - Do they have any hobbies?: I like to play video games, draw and read!");
+		System.out.println("4 - Do they have any plans for the fall?: The only real plans I have is to get a part time winter job and celebrate Halloween.");
+		System.out.println("5 - Anything else they'd like the class to know about them: There's nothing much, I'm just a nerd who wants to spend her days being happy!");
+		
+	}
+
+}
